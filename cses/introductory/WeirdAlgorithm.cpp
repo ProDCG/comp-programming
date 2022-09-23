667f6ae8a051f2cf8f7fa7bce9cb9f34a573a8a4
Mason Stuart <mason.stuart05@outlook.com>
2022-09-22 22:50:52 -0700

    CSES/Introductory, WeirdAlgorithm.cpp

--- cses/introductory/WeirdAlgorithm.cpp
+++ cses/introductory/WeirdAlgorithm.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main() {
-    
+    int n;
+    cin >> n;
+    while (n != 1) {
+        cout << n << ' ';
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = n * 3 + 1;
+        }
+    }
+    cout << 1;
     return 0;
 }
