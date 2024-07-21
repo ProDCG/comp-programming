5c4b680d1a84220bd08bbc7ab40374d4fe16aad0
Mason Stuart <mason.stuart05@outlook.com>
2024-07-20 21:12:40 -0700

    valid sudoku

--- neetcode/arrays_hashing/valid_sudoku.cpp
+++ neetcode/arrays_hashing/valid_sudoku.cpp
@@ -1,10 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_map<int, unordered_set<char>> rows;
+        unordered_map<int, unordered_set<char>> cols;
+        unordered_map<int, unordered_set<char>> square;
 
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                char num = board[r][c];
+                if (num == '.') continue;
 
+                if (rows[r].count(num) || cols[c].count(num))
 
-    return 0;
-}
+                rows[r].insert(num);
+                cols[c].insert(num);
+            }
+        }
+    }
+};
