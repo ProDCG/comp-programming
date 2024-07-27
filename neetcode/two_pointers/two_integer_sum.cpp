2a42ce4bbca4442391f15368e2e0d2e13871a229
Mason Stuart <mason.stuart05@outlook.com>
2024-07-27 08:41:42 -0700

    two integer sum

---
+++ neetcode/two_pointers/two_integer_sum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0, r = numbers.size() - 1;
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum < target) {
+                l++;
+            } else if (sum > target) {
+                r--;
+            }
+            else {
+                return {l + 1, r + 1};
+            }
+        }
+        return {};
+    }
+};
