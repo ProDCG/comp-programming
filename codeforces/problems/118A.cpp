e6a318b54797b141147aa964edf8fc6e83d43bab
Mason Stuart <mason.stuart05@outlook.com>
2024-08-13 07:33:07 -0700

    118a

--- codeforces/problems/118A.cpp
+++ codeforces/problems/118A.cpp
@@ -4,7 +4,18 @@ using namespace std;
 #define ll long long
 
 int main() {
+    string vowels = "AOYEUI";
+    string input;
+    cin >> input;
 
+    for (int i = 0; i < input.size(); i++) {
+        if (vowels.find(input[i]) != vowels.npos) {
+            input = input.substr(0, i) + input.substr(i + 1, input.size());
+            i--;
+        } else if (isupper(vowels.find(input[i]))) {
+            input[i] = tolower(vowels.find(input[i]));
+        }
+    }
 
     return 0;
 }
