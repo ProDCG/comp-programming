2c0cda44754deaedd2c7c4a366c52cbe450233d2
Mason Stuart <mason.stuart05@outlook.com>
2024-09-30 00:24:48 -0400

    lc interview prep 27

---
+++ leetcode/to150/27.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// O(n) time, O(1) space
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        return index;
+    }
+};
