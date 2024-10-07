73ddc50768dfd0cad9d91cee491157d3dee26a74
Mason Stuart <mason.stuart05@outlook.com>
2024-10-06 22:19:37 -0700

    reorer

---
+++ neetcode/linked_list/reorder_linked_list.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* second = slow->next;
+        ListNode* prev = slow->next = nullptr;
+
+        while (second != nullptr) {
+            ListNode* tmp = second->next;
+            second->next = prev;
+            prev = second;
+            second = tmp;
+        }
+
+        ListNode* first = head;
+        second = prev;
+        while (second != nullptr) {
+            ListNode* tmp1 = first->next;
+            ListNode* tmp2 = second->next;
+
+            first->next = second;
+            second->next = tmp1;
+
+            first = tmp1;
+            second = tmp2;
+        }
+    }
+};
