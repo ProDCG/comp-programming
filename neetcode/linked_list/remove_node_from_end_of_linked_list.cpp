274a54552bc68232c778241ba66bfddd9c83a1c8
Mason Stuart <mason.stuart05@outlook.com>
2024-10-06 22:42:32 -0700

    remove node

---
+++ neetcode/linked_list/remove_node_from_end_of_linked_list.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* left = dummy;
+        ListNode* right = head;
+
+        while (n > 0) {
+            right = right->next;
+            n--;
+        }
+
+        while (right != nullptr) {
+            left = left->next;
+            right = right->next;
+        }
+
+        left->next = left->next->next;
+        return dummy->next;
+    }
+};
